d7746394684e6d860f4ad0c951d00fddef96917a
Fernando Herrera <fernando.herrera85@gmail.com>
2022-11-02 10:50:26 -0400

    updated

--- src/api/githubApi.ts
+++ src/api/githubApi.ts
@@ -4,7 +4,7 @@ import axios from 'axios';
 export const githubApi = axios.create({
     baseURL: 'https://api.github.com/repos/facebook/react',
     headers: {
-        Authorization: 'Bearer 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'
+        Authorization: 'Bearer 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'
     }
 });
 
