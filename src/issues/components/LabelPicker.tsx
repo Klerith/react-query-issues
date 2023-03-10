513d0afcaa4e93a6a63f83c3f698f789e73c18d8
Carlos <carlos_dev@slumadrid.slu.edu>
2023-03-10 12:01:01 +0100

    fin seccion 3

--- src/issues/components/LabelPicker.tsx
+++ src/issues/components/LabelPicker.tsx
@@ -1,7 +1,13 @@
 import { useLabels } from "../hooks/useLabels";
 import { LoadingIcon } from '../../share/components/LoadingIcon';
+import { FC } from 'react';
 
-export const LabelPicker = () => {
+interface Props {
+  selectedLabels: string[];
+  onChange: (labelName: string) => void;
+}
+
+export const LabelPicker: FC<Props> = ({selectedLabels, onChange}) => {
   const labelsQuery = useLabels();
 
   if (labelsQuery.isLoading)
@@ -10,8 +16,9 @@ export const LabelPicker = () => {
       <div>
         {labelsQuery.data?.map(label => (
           <span key={label.id}
-            className="badge rounded-pill m-1 label-picker"
-            style={{ border: `1px solid #${label.color}`, color: `${label.color}` }}
+            className={`badge rounded-pill m-1 label-picker ${selectedLabels.includes(label.name)? 'label-active' : ''}`}
+            style={{ border: `1px solid #${label.color}`, color: `#${label.color}` }}
+            onClick = { () => onChange(label.name)}
           >
             {label.name}
           </span>
--- src/issues/hooks/useLabels.tsx
+++ src/issues/hooks/useLabels.tsx
@@ -1,21 +1,35 @@
-import { Label } from '../interfaces/label';
-import { githubApi } from '../../api/githubApi';
-import { useQuery } from '@tanstack/react-query';
-import { sleep } from '../../helpers/sleep';
+import { useQuery } from "@tanstack/react-query";
+import { githubApi } from "../../api/githubApi";
+import { sleep } from "../../helpers/sleep";
+import { Label } from "../interfaces/label";
 
-const getLabels = async():Promise<Label[]> => {
-    await sleep(2)
-    const { data } = await githubApi.get<Label[]>('/labels')
-    return data;
-  }
+const getLabels = async (): Promise<Label[]> => {
+  await sleep(2);
+  const { data } = await githubApi.get<Label[]>("/labels");
+  return data;
+};
 
-export const useLabels = () =>{
-    const labelsQuery = useQuery(
-        ['labels'],
-        getLabels,
-        {
-           staleTime: 1000 * 60 * 60 
-        }
-      );
-return labelsQuery;
-}
+export const useLabels = () => {
+  const labelsQuery = useQuery(["labels"], getLabels, {
+     staleTime: 1000 * 60 * 60,
+    placeholderData: [
+      {
+        id: 725156255,
+        node_id: "MDU6TGFiZWw3MjUxNTYyNTU=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue%20(taken)",
+        name: "good first issue (taken)",
+        color: "b60205",
+        default: false,
+      },
+      {
+        id: 717031390,
+        node_id: "MDU6TGFiZWw3MTcwMzEzOTA=",
+        url: "https://api.github.com/repos/facebook/react/labels/good%20first%20issue",
+        name: "good first issue",
+        color: "6ce26a",
+        default: true,
+      },
+    ],
+  });
+  return labelsQuery;
+};
